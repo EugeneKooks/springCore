1fc97f5c24214f803f53f957b84a4dcd59f34aec
EugeneKooks <eugene.kooks@gmail.com>
2017-11-03 08:23:55 +0300

    Initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,32 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+    private Client client;
+
+    private EventLogger eventLogger;
+
+    public static void main(String[] args) {
+
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
+        App app = (App) ctx.getBean("app");
+
+        Event event = ctx.getBean(Event.class);
+        app.logEvent(event, "Some event for 1");
+
+        event = ctx.getBean(Event.class);
+        app.logEvent(event, "Some event for 2");
+    }
+
+    public App(Client client, EventLogger eventLogger) {
+        super();
+        this.client = client;
+        this.eventLogger = eventLogger;
+    }
+
+    private void logEvent(Event event, String msg) {
+        String message = msg.replaceAll(client.getId(), client.getFullName());
+        event.setMsg(message);
+        eventLogger.logEvent(event);
+    }
+}
